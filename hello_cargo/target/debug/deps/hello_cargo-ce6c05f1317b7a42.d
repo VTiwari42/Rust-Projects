7573929a35b425775fe96c8e94c3bd6834c9b3e3
Vivan Tiwari <tiwari42@purdue.edu>
2024-04-17 01:47:49 -0400

    Hello World Completed

---
+++ hello_cargo/target/debug/deps/hello_cargo-ce6c05f1317b7a42.d
@@ -0,0 +1,5 @@
+/home/tnt/Rust-Projects/hello_cargo/target/debug/deps/hello_cargo-ce6c05f1317b7a42: src/main.rs
+
+/home/tnt/Rust-Projects/hello_cargo/target/debug/deps/hello_cargo-ce6c05f1317b7a42.d: src/main.rs
+
+src/main.rs:
---
+++ hello_cargo/target/debug/hello_cargo.d
@@ -0,0 +1,1 @@
+/home/tnt/Rust-Projects/hello_cargo/target/debug/hello_cargo: /home/tnt/Rust-Projects/hello_cargo/src/main.rs
